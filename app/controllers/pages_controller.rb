794e4ac330c60ba8aa3624e5d00f91e3870014c8
Islam Askar <islam@islam-tablet.localhost>
2014-05-15 01:08:26 +0200

    intial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def home
+    @title = "Home Page"
+  end
+
+  def contact
+  end
+  
+  def about
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlexApp::Application.config.secret_token = '93ebf6ae1ce265ac723355b40ab6ae0ab9d6ea94e4f59c884528e63ef723960be90993d5f22dfe8592c450877c46c33736b03185bb2b646ad628c8934b787c5b'
